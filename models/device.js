b03f89b67bed4c045c86801b52bcbad9f06328ba
ChenJunJian <chenjunjian@eshinetech.com>
2016-06-06 17:58:03 +0800

    modify device model for a test
    This commit refs #905.
    just for a test.
    
    HOW BUILT
    npm i
    
    HOW VERIFIED
    see models/device
    
    Change-Id: I7a6c83ba3f8e503ba62bbd062f48069f3e776810
    Signed-off-by: ChenJunJian <chenjunjian@eshinetech.com>

--- models/device.js
+++ models/device.js
@@ -12,7 +12,7 @@ var deviceSchema = new Schema({
   macAddress:String,  //设备的 MAC 地址	unique
   name:String,  //设备型号 id	fk: DeviceModel
   bbcloudDeviceId:String,  //bbcloud 的设备 id，由规则生成	unique, index
-  wechatDeviceId:String,  //微信硬件平台分配的设备 id	unique
+  wechatDeviceId:Number,  //微信硬件平台分配的设备 id	unique
   wechatDeviceQrticket:String,  //设备二维码生产串
   wechatDeviceLicence:String, //产品使用直连SDK时返回的设备证书
   aliyunDeviceId:String,  //阿里云 IoT 服务分配的设备 id	unique
